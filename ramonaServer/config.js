b71c20498f2ac9bfb7a8344d549ebaba2816719a
Anabella <shokoufeh.namazian@gmail.com>
2022-07-14 18:06:19 -0700

    full

---
+++ ramonaServer/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/ramonaserver',
+    'facebook': {
+        clientId: '435672821683794',
+        clientSecret: '8443785ac70fc0fc9d091c2070202443'
+    }
+}
